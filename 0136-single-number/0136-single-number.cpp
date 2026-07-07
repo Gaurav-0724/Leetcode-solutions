22e0b12f619e712684fb4828b1722a06c1b7f0d1
Gaurav-0724 <gauravsaini24122007@gmail.com>
2026-07-07 18:38:07 +0530

    Time: 12 ms (11.68%), Space: 20.7 MB (24.16%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int digit = 0;
+        
+        for(int i=0;i<32;i++){
+            int noo= 0;
+            int noz= 0;
+            for(int i=0;i<n;i++){
+                if((nums[i]&1)==0) noz+=1;
+                else noo+=1;
+                nums[i]>>=1;
+            }
+            if(noo%2==1){
+                int t = 1<<i;
+                digit = (digit|t);
+
+            } 
+            else digit = (digit|0);
+           
+
+
+        }
+        return digit;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
