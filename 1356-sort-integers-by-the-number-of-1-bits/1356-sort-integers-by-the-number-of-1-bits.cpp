229cbe61bbe0c3988d3a85402eae3227fdc44f63
Gaurav-0724 <gauravsaini24122007@gmail.com>
2026-07-08 22:44:17 +0530

    Time: 8 ms (15.8%), Space: 16.7 MB (5.15%) - LeetHub - Time: O(, Space: O(?)

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    vector<int> sortByBits(vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int>> bits;
+        for(int i=0;i<n;i++){
+
+            bits.push_back({__builtin_popcount(arr[i]),arr[i]});
+
+        }
+        sort(bits.begin(),bits.end());
+        vector<int>ans;
+        for(int i = 0 ; i<n ; i++){
+            ans.push_back(bits[i][1]);
+        }
+        return ans;
+
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
