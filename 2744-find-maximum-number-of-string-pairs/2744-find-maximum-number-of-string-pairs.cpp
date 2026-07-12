5d3fb1d7d04d4bf203e4399b756783984812c3d3
Gaurav-0724 <gauravsaini24122007@gmail.com>
2026-07-12 17:07:04 +0530

    Time: 0 ms (100%), Space: 25.6 MB (50.74%) - LeetHub - Time: O(n*m), Space: O(n)

--- 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.cpp
+++ 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.cpp
@@ -3,21 +3,28 @@ public:
     int maximumNumberOfStringPairs(vector<string>& words) {
         int n = words.size();
         unordered_set<string> s;
-        
-        for(int i=0;i<n;i++){
-            s.insert(words[i]);
-        }
         int count=0;
         for(int i=0;i<n;i++){
             string rev = words[i];
             reverse(rev.begin(),rev.end());
-
-            if(rev==words[i]) continue;
-            if(s.find(rev)!=s.end()){
+            if(s.find(rev)==s.end()){
+                s.insert(words[i]);
+            }
+            else{
                 count++;
-                s.erase(words[i]);
             }
         }
+        
+        // for(int i=0;i<n;i++){
+        //     string rev = words[i];
+        //     reverse(rev.begin(),rev.end());
+
+        //     if(rev==words[i]) continue;
+        //     if(s.find(rev)!=s.end()){
+        //         count++;
+        //         s.erase(words[i]);
+        //     }
+        // }
         return count;
         
     }
