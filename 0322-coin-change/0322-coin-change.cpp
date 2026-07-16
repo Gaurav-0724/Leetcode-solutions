a78862662dd4cf45e650345aa3e87e58f24f751a
Gaurav-0724 <gauravsaini24122007@gmail.com>
2026-07-16 20:55:43 +0530

    Time: 47 ms (34.74%), Space: 52.9 MB (12.46%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -16,10 +16,26 @@ public:
 
         int n= coins.size();
         vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
-        int answer = findMinCoin(n,amount,coins,dp);
+        // int answer = findMinCoin(n,amount,coins,dp);
 
-        return answer>=1e9 ? -1: answer;
-        
+        // return answer>=1e9 ? -1: answer;
+        for(int j=0;j<=amount;j++){
+            dp[0][j] = 1e9;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(j-coins[i-1]>=0){
+                    dp[i][j] = min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount]>=1e9? -1: dp[n][amount];
     }
 };
 
