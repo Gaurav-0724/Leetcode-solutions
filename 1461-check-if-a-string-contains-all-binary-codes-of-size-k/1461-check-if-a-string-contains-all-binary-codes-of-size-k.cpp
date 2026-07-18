724a0a12f7aee638d2f19452768a8546f4b32705
Gaurav-0724 <gauravsaini24122007@gmail.com>
2026-07-18 21:21:52 +0530

    Time: 499 ms (33.93%), Space: 150.6 MB (16.83%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        unordered_set<string> st;
+
+        int n = s.length();
+
+        int codes = 1<<k;
+
+        for(int i=k;i<=n;i++){
+            string sub = s.substr(i-k, k);
+
+            if(!st.count(sub)){
+                st.insert(sub);
+                codes--;
+            }
+            if(codes==0){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
