c2e1ef40fe521515d33092386820f64deeff83ed
Gaurav-0724 <gauravsaini24122007@gmail.com>
2026-07-28 17:38:34 +0530

    Added solution - Time: O(m * n), Space: O(m * n)

---
+++ 704022-minimum-number-of-deletions-and-insertions-GFG/704022-minimum-number-of-deletions-and-insertions.cpp
@@ -0,0 +1,29 @@
+class Solution {
+
+  public:
+    
+    int minOperations(string &s1, string &s2) {
+        // Your code goes here
+        int m =s1.size(), n = s2.size();
+        
+        
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j] = 1+ dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        return (m-dp[m][n]) +(n-dp[m][n]);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
