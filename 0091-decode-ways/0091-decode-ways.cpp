ef35915fa6d7fa2ddf25d2a7a5214f036bec2420
Gaurav-0724 <gauravsaini24122007@gmail.com>
2026-07-29 16:45:36 +0530

    Time: 0 ms (100%), Space: 8.9 MB (36.15%) - LeetHub - Time: O(n), Space: O(n)

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -30,8 +30,29 @@ public:
     int numDecodings(string s) {
 
         int n = s.size();
-        vector<int> dp(n+1,-1);
-        return solve(0,n,s,dp);
+        vector<int> dp(n+1,0);
+        // return solve(0,n,s,dp);
+
+        dp[n] =1;
+
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0'){
+                dp[i] =0;
+            }
+            else{
+                 dp[i] = dp[i+1];
+
+                if(i+1<n){
+                    int num =(s[i]-'0')*10+ (s[i+1]-'0');
+                    if(num>=10 && num<=26){
+                        dp[i]+= dp[i+2];
+                    }
+                }
+            }
+
+            
+        }
+        return dp[0];
 
         
     }
