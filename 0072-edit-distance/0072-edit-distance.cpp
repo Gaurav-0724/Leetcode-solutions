630ad9edc48fc7c5f69465262d1373c4249da004
Gaurav-0724 <gauravsaini24122007@gmail.com>
2026-08-01 09:02:49 +0530

    Time: 6 ms (68.73%), Space: 13.1 MB (60.91%) - LeetHub - Time: O(m*n), Space: O(m*n)

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -27,9 +27,33 @@ public:
     int minDistance(string word1, string word2) {
 
         int m = word1.size(), n=word2.size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
 
-        return minsteps(m,n,word1,word2,dp);
+        for(int j=1;j<=n;j++){
+            dp[0][j] = j;
+        }
+
+        for(int i=1;i<=m;i++){
+            dp[i][0] = i;
+        }
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+
+                else{
+                    int inserted = 1+ dp[i][j-1];
+                    int deleted = 1+ dp[i-1][j];
+                    int replaced = 1+ dp[i-1][j-1];
+                    dp[i][j] = min(inserted,min(deleted,replaced));
+                }
+            }
+        }
+        return dp[m][n];
+
+        // return minsteps(m,n,word1,word2,dp);
         
     }
 };
