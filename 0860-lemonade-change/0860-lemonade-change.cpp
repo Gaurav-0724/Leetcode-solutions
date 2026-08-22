04a093102180e069ed997a775a71d1ce60b5c5a5
Gaurav-0724 <gauravsaini24122007@gmail.com>
2026-08-22 23:13:24 +0530

    Time: 0 ms (100%), Space: 87.1 MB (96.64%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (int bill : bills) {
+
+            if (bill == 5) {
+                five++;
+            }
+
+            else if (bill == 10) {
+                if (five == 0)
+                    return false;
+
+                five--;
+                ten++;
+            }
+
+            else { 
+
+               
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+
+               
+                else if (five >= 3) {
+                    five -= 3;
+                }
+
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
